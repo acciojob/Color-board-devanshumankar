c71ae7303773339000d506b7a7689726c29ad5f3
Devanshu Chandu Mankar <158685271+devanshumankar@users.noreply.github.com>
2025-11-19 14:44:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.querySelectorAll(".square").forEach(square => {
+  square.addEventListener("mouseover", () => {
+    square.classList.add("hovered");
+  });
+
+  square.addEventListener("mouseout", () => {
+    square.classList.remove("hovered");
+  });
+});
